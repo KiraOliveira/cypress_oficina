0596dbf225c62f4000880b87e178f9d56d246b29
Kira <kira.oliveira.web@gmail.com>
2022-08-22 16:14:03 -0300

    oficina de cypress

---
+++ cypress/support/commands.js
@@ -0,0 +1,38 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (email, password) => {
+
+    //it.only('Login com sucesso no front.serverest', () => {
+        cy.visit('https://front.serverest.dev/')
+        cy.get('[data-testid="email"]').type('Elody67@hotmail.com')
+        cy.get('[data-testid="senha"]').type('UT6g42nZEbjmT7D')
+        cy.get('[data-testid="entrar"]').click()
+        cy.get('.imagem').should('be.visible') //assertion na doc do cypress
+        cy.get('.imagem')
+    
+      })  
+    
