019f9427c32c5753f9e45e681718a9fbd539f0a8
Kira Oliveira <kira.oliveira.web@gmail.com>
2024-05-16 05:38:59 -0300

    Corrigindo arquivo spec

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,13 +1,13 @@
 //import {faker} from '@faker-js/faker'
-import usersPage from '../support/pages/usersPage'
+//import usersPage from '../support/pages/usersPage'
 
 /// <reference types="Cypress" />
 
 describe('Testes no front end da ServeRest', () => {
 
-    beforeEach('Login com sucesso no front.serverest', () => //{
+    beforeEach('Login com sucesso no front.serverest', () => {
       cy.login()    
-    );
+    })
 
     it('Deve acessar a lista de usuário e validar um usuário cadastrado', () => {
     
